c725a2b1fdc81279c0c244ccdf178cb205506266
Nelson Rodrigues <nelson@nelsonjrodrigues.com>
2016-01-23 12:31:37 -0800

    Implement timings parsing

--- src/main/java/com/nelsonjrodrigues/pchud/net/PcMessage.java
+++ src/main/java/com/nelsonjrodrigues/pchud/net/PcMessage.java
@@ -1,11 +1,55 @@
 package com.nelsonjrodrigues.pchud.net;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(fluent = true)
 public class PcMessage {
 
     private int buildVersionNumber;
     private int packetType;
+
+    // TelemetryData
+
+    // Game states
+    private int gameSessionState;
+
+    // Participant info
+    private int viewedParticipantIndex;
+    private int numParticipants;
+
+    // Unfiltered input
+    private int unfilteredThrottle;
+    private int unfilteredBrake;
+    private int unfilteredSteering;
+    private int unfilteredClutch;
+    private int raceStateFlags;
+
+    // Event information
+    private int lapsInEvent;
+
+    // Timings
+    private float bestLapTime;
+    private float lastLapTime;
+    private float currentTime;
+    private float splitTimeAhead;
+    private float splitTimeBehind;
+    private float splitTime;
+    private float eventTimeRemaining;
+    private float personalFastestLapTime;
+    private float worldFastestLapTime;
+    private float currentSector1Time;
+    private float currentSector2Time;
+    private float currentSector3Time;
+    private float fastestSector1Time;
+    private float fastestSector2Time;
+    private float fastestSector3Time;
+    private float personalFastestSector1Time;
+    private float personalFastestSector2Time;
+    private float personalFastestSector3Time;
+    private float worldFastestSector1Time;
+    private float worldFastestSector2Time;
+    private float worldFastestSector3Time;
 
 }
--- src/main/java/com/nelsonjrodrigues/pchud/net/PcMessageParser.java
+++ src/main/java/com/nelsonjrodrigues/pchud/net/PcMessageParser.java
@@ -4,12 +4,46 @@ public class PcMessageParser {
 
     public PcMessage fromByteArray(byte[] buffer, int offset) {
         Extractor e = new Extractor(buffer, offset);
+
         PcMessage message = new PcMessage();
 
-        message.setBuildVersionNumber(e.u16());
+        message.buildVersionNumber(e.u16())
+               .packetType(e.u8() & 0x3);
 
-        int packetType = e.u8() & 0x3;
-        message.setPacketType(packetType);
+        message.gameSessionState(e.u8());
+
+        message.viewedParticipantIndex(e.u8())
+               .numParticipants(e.u8());
+
+        message.unfilteredThrottle(e.u8())
+               .unfilteredBrake(e.u8())
+               .unfilteredSteering(e.u8())
+               .unfilteredClutch(e.u8())
+               .raceStateFlags(e.u8());
+
+        message.lapsInEvent(e.u8());
+
+        message.bestLapTime(e.f32())
+               .lastLapTime(e.f32())
+               .currentTime(e.f32())
+               .splitTimeAhead(e.f32())
+               .splitTimeBehind(e.f32())
+               .splitTime(e.f32())
+               .eventTimeRemaining(e.f32())
+               .personalFastestLapTime(e.f32())
+               .worldFastestLapTime(e.f32())
+               .currentSector1Time(e.f32())
+               .currentSector2Time(e.f32())
+               .currentSector3Time(e.f32())
+               .fastestSector1Time(e.f32())
+               .fastestSector2Time(e.f32())
+               .fastestSector3Time(e.f32())
+               .personalFastestSector1Time(e.f32())
+               .personalFastestSector2Time(e.f32())
+               .personalFastestSector3Time(e.f32())
+               .worldFastestSector1Time(e.f32())
+               .worldFastestSector2Time(e.f32())
+               .worldFastestSector3Time(e.f32());
 
         return message;
     }
