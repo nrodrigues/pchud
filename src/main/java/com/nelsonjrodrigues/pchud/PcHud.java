e0477265ca34f3b3ab747641eabf8f622d816f31
Nelson Rodrigues <nelson@nelsonjrodrigues.com>
2016-01-23 15:23:52 -0800

    Add Worlds model to main class

--- src/main/java/com/nelsonjrodrigues/pchud/PcHud.java
+++ src/main/java/com/nelsonjrodrigues/pchud/PcHud.java
@@ -5,21 +5,32 @@ import java.io.IOException;
 import com.nelsonjrodrigues.pchud.net.MessageListener;
 import com.nelsonjrodrigues.pchud.net.NetThread;
 import com.nelsonjrodrigues.pchud.net.PcMessage;
+import com.nelsonjrodrigues.pchud.world.Worlds;
 
 import lombok.extern.slf4j.Slf4j;
+
+
 
 @Slf4j
 public class PcHud {
 
     public static void main(String[] args) {
 
+
+        Worlds worlds = new Worlds();
+        worlds.addPropertyChangeListener(evt -> {
+            log.debug("{}", worlds);
+        });
+
         try {
             NetThread netThread = new NetThread();
             netThread.addMessageListener(new LogMessageListener());
+            netThread.addMessageListener(worlds);
             netThread.start();
 
             netThread.join();
-        } catch (IOException | InterruptedException e) {
+        }
+        catch (IOException | InterruptedException e) {
             log.error(e.getMessage(), e);
         }
         log.debug("Exiting");
